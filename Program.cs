748552ddd3eb31150eac15d7a3f18d5a227201a7
Trevias Xk <trevias@live.com>
2024-11-28 10:39:08 -0300

    Version 1.0.0.0 published.

---
+++ Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+internal class Program{
+
+    // Locations files
+    static string filePlayGTAV = @".\PlayGTAV.exe";
+    static string fileGTAV = @".\GTA5.exe";
+    static string fileVersion = @".\version.txt";
+    static string fileServiceLog = "C:/ProgramData/Rockstar Games/Launcher/service_log.txt";
+
+    // CommandLines addicionals
+    static string commandLines = "";
+    static int lastLine = 0;
+
+    static bool byPass = false, isUpdating = false;
+
+    private static void Main(string[] args){
+        foreach(var line in Environment.GetCommandLineArgs().Skip(1))
+            commandLines += " " + line;
+        ShowCredits();
+        StartRockstarLauncher();
+        StartService();
+    }
+
+    static void StartRockstarLauncher(){
+        if(!File.Exists(filePlayGTAV)){
+            Console.WriteLine("[ERROR] {0} not found!", filePlayGTAV);
+            Environment.Exit(0);
+        }else{
+            Console.WriteLine("[DEBUG] Running Rockstar Launcher...");
+            Process.Start(filePlayGTAV, commandLines);
+        }
+    }
+
+    // Initializing check service...
+    static void StartService() => new Thread(new ThreadStart(CheckRockstarService)).Start();
+
+    static void CheckRockstarService(){
+        while(true){
+            Thread.Sleep(1000);
+            if(File.Exists(fileServiceLog)){
+                // Load all lines in array
+                var Lines = File.ReadAllLines(fileServiceLog);
+
+                // Value initial to LastLines
+                if(lastLine == 0)
+                    lastLine = Lines.Length;
+
+                // Load logs
+                string logs = "";
+                if(Lines.Length > lastLine)
+                    foreach(var line in Lines.Skip(lastLine))
+                        logs += line;
+
+                // Check service stopped
+                if(logs.Contains("Setting state to SERVICE_STOP")){
+                    // Update finish
+                    if(isUpdating){
+                        isUpdating = false; 
+                        byPass = false;
+                    }else{
+                        Console.WriteLine("[ERROR] Rockstar Service not initialized!");
+                        break;
+                    }
+                }
+
+                // Check Rockstar Launcher is updating
+                if(!isUpdating && logs.Contains(@"Rockstar Games\Launcher\index.bin")){
+                    Console.WriteLine("[DEBUG] Rockstar Launcher is updating...");
+                    isUpdating = true;
+                }
+
+                // Start Game
+                if(byPass && !isUpdating){
+                    if(!File.Exists(fileGTAV))
+                        Console.WriteLine("[ERROR] {0} not found!", fileGTAV);
+                    else{
+                        Console.WriteLine("[DEBUG] Running GTA V...");
+                        Process.Start(fileGTAV, "-useEpic -fromRGL -EpicPortal");
+
+                        if(Process.GetProcessesByName("Launcher") is Process[] process){
+                            // Minimize Rockstar Launcher                                    
+                        }
+                    }
+                    break;
+                }
+
+                // Check service running
+                if(logs.Contains("Setting state to SERVICE_RUNNING")){
+                    if(!byPass && !isUpdating){
+                        byPass = true;
+                        Console.WriteLine("[DEBUG] Running Rockstar Service...");
+                    }
+                }
+                
+                // Update LastLine
+                lastLine = Lines.Length;
+            }
+        }
+    }
+
+    static void ShowCredits(){
+        Console.WriteLine("================= Play GTA V Linux =================");
+        Console.WriteLine("Store:   \t \t {0}", commandLines.Contains("-epicAppId=gta5") ? "Epic Games" : commandLines.Contains("-steamAppId=gta5") ? "Steam" : "Unknown");
+        Console.WriteLine("Battle Eye: \t \t {0}", commandLines.Contains("-nobattleye") ? "Disabled" : "Enabled");
+        Console.WriteLine("Github: \t \t https://github.com/treviasxk");
+        Console.WriteLine("Repository: \t \t https://github.com/treviasxk/PlayGTAVLinux");
+        if(File.Exists(fileVersion))
+            Console.WriteLine("GTA Version: \t \t {0}", File.ReadAllText(fileVersion));
+        Console.WriteLine("Version: \t \t 1.0.0.0");
+        Console.WriteLine("Created by: \t \t Trevias Xk");
+        Console.WriteLine("====================================================");
+        Console.WriteLine("");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# PlayGTAVLinux
-Executable application to fix GTA V startup on Linux.
+# Play GTA V Linux
+![Preview](/screenshots/banner.jpg)
+
+If you are having trouble launching your GTA V purchased from Epic Games with the Heroic Games Launcher, you can use this alternative executable to launch your game.
+
+## Installation
+1 - Download the *PlayGTAVLinux.exe* executable from the [releases](https://github.com/treviasxk/PlayGTAVLinux/releases) page.
+
+2 - Move the downloaded file to the GTAV root folder.
+
+3 - Go to the Heroic Games Launcher, in GTAV settings->advanced->Select Alternative EXE, select *PlayGTAVLinux.exe* inside the GTAV folder.
+
+## FAQ
+* Does it work on Steam too? - *Not tested yet*
+* Will you create a patch for RDR2 too? - *I haven't purchased the game yet, but if I get donations from this project, I will.*
+
+## Donate
+*Please consider donating if I have helped you, so that I can continue to bring you updates.*
+ - Paypal: trevias@live.com
